76fff629ca7af64bc3aa9b6d760b14a909bfec26
张 儒杰 <zhangrujie@jltchina.com>
2023-11-02 17:32:32 +0800

    feat: 首页完成

---
+++ src/app/evaluation/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Evaluation() {
+  return (
+    <main>
+      <div>Evaluation</div>
+    </main>
+  )
+}
---
+++ src/app/toilet/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Toilet() {
+  return (
+    <main>
+      <div>Toilet</div>
+    </main>
+  )
+}
