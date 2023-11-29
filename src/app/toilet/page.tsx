eef6775d113cb6fee3f1747a33bf4b8ff9b38e8f
张 儒杰 <zhangrujie@jltchina.com>
2023-11-29 19:06:31 +0800

    no message

--- src/app/toilet/page.tsx
+++ src/app/toilet/page.tsx
@@ -3,7 +3,7 @@ import { Button, TContainer } from '@/components'
 export const revalidate = 60
 
 import { fetchToiletList } from '@/libs'
-import { List, ListItem } from '@mui/material'
+import { Box, List, ListItem } from '@mui/material'
 
 import React from 'react'
 import ToiletItem from './ToiletItem'
@@ -11,21 +11,31 @@ import ToiletItem from './ToiletItem'
 import { LinkWrapper } from '@/components'
 
 export default async function Toilet() {
-  const { records } = await fetchToiletList()
+  // const { records } = await fetchToiletList()
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos')
+  const list = await res.json()
 
   return (
-    <TContainer>
-      <List>
-        {(records ?? []).map((item: any) => {
-          return (
-            <LinkWrapper href={'/toilet/detail'} key={item.toiletNumber}>
-              <ListItem>
-                <ToiletItem data={item} />
-              </ListItem>
-            </LinkWrapper>
-          )
-        })}
-      </List>
-    </TContainer>
+    <Box>
+      {list.map((item: any) => {
+        return <Box key={item.userId}>{item?.title}</Box>
+      })}
+    </Box>
   )
+
+  // return (
+  //   <TContainer>
+  //     <List>
+  //       {(records ?? []).map((item: any) => {
+  //         return (
+  //           <LinkWrapper href={'/toilet/detail'} key={item.toiletNumber}>
+  //             <ListItem>
+  //               <ToiletItem data={item} />
+  //             </ListItem>
+  //           </LinkWrapper>
+  //         )
+  //       })}
+  //     </List>
+  //   </TContainer>
+  // )
 }
